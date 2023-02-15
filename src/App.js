a088465c249de5a3aa985ebff29d7151bfc4a289
Felipe Alves <felipealves3580@gmail.com>
2023-02-15 14:28:42 -0300

    Add div

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>ToDoList</h1>
+      <div className="addTask">
+        <input placeholder="Digite o nome da tarefa" />
+        <button>Adicionar Tarefa</button>
+      </div>
+      <div className="items"></div>
     </div>
   );
 }
